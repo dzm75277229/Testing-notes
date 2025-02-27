49064573eacc05b9318908549602660e907a2147
dzm75277229 <87660705+dzm75277229@users.noreply.github.com>
2025-02-27 21:59:50 +0800

    Update TowSum.c

--- notes/LeetCode_Python/TowSum.py
+++ notes/LeetCode_Python/TowSum.py
@@ -1,1 +1,14 @@
-
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int *a;
+    a=(int *)malloc(2*sizeof(int));//用a数组返回结果
+    for(int i=0;i<numsSize;i++){
+        for(int j=i+1;j<numsSize;j++){
+            if(nums[i]+nums[j]==target){
+                a[0]=i;
+                a[1]=j;
+                break;
+            }
+        }
+    }
+    return a;
+}
